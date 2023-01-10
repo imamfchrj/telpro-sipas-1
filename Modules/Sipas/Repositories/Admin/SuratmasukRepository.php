34719064aed64eb0289ed71f4b572cf8a3b9ddc1
imamfchrj <imam.fchrj@gmail.com>
2023-01-10 18:31:18 +0700

    notifikasi surat masuk

--- Modules/Sipas/Repositories/Admin/SuratmasukRepository.php
+++ Modules/Sipas/Repositories/Admin/SuratmasukRepository.php
@@ -9,9 +9,12 @@
 namespace Modules\Sipas\Repositories\Admin;
 
 use DB;
+use App\Models\User;
 use Modules\Sipas\Entities\MSuratmasuk;
 use Modules\Sipas\Entities\MUnit;
 use Modules\Sipas\Repositories\Admin\Interfaces\SuratmasukRepositoryInterface;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
 
 class SuratmasukRepository implements SuratmasukRepositoryInterface
 {
@@ -51,7 +54,7 @@ class SuratmasukRepository implements SuratmasukRepositoryInterface
         }
 
         if ($perPage) {
-            return $suratmasuk->sortable()->orderBy('id','DESC')->paginate($perPage);
+            return $suratmasuk->sortable()->orderBy('id', 'DESC')->paginate($perPage);
         }
 
         return $suratmasuk->get();
@@ -84,6 +87,14 @@ class SuratmasukRepository implements SuratmasukRepositoryInterface
         $suratmasuk->created_by_name = auth()->user()->name;
         $suratmasuk->status_id = 1;
         $suratmasuk->status = 'Pending Received';
+
+        $unit_id_tujuan = $unit_id;
+        $user_units = User::where('group', $unit_id_tujuan)->get();
+        foreach ($user_units as $user) {
+            $no_telp_pic = $user->nomor_tlp;
+            $nama_pic = $user->name;
+            $this->send_notifikasi($suratmasuk, $no_telp_pic, $nama_pic);
+        }
         return $suratmasuk->save();
     }
 
@@ -145,7 +156,7 @@ class SuratmasukRepository implements SuratmasukRepositoryInterface
         }
 
         if ($perPage) {
-            return $suratmasuk->sortable()->orderBy('id','DESC')->paginate($perPage);
+            return $suratmasuk->sortable()->orderBy('id', 'DESC')->paginate($perPage);
         }
 
         return $suratmasuk->get();
@@ -162,5 +173,49 @@ class SuratmasukRepository implements SuratmasukRepositoryInterface
         $suratmasuk->status_id = 2;
         $suratmasuk->status = 'Received';
         return $suratmasuk->save();
+    }
+
+    public function send_notifikasi($params,$no_tlp_pic, $nama_pic)
+    {
+        $url = 'https://wa01.ocatelkom.co.id/api/v2/push/message';
+        $token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiYXBwbGljYXRpb24iOiI2MzYzNzU5ZTBjOTQ5NDAwMjE3NjM0YTkiLCJpYXQiOjE1MTYyMzkwMjJ9.dCsEsnctWvZfsu9OGYGKCQW5u0-oRAnAI7806-4Dl0ea57kgggTY7rC5pJYwtfabOybcM5loP95Bam_CTkQ4l2Nm_yxiRBDTT-xfq8uC1JwKclZu0ZS2ekjO-MXuk08tntnXCpi-gTVvAuYno1QaFgpsMFed6HuQB60IlHyxGH9CTnA7Nsfyc0vCI2KH9px2MhwIOWTsN8p_GRE-yk80eOVnAwMGQ3JoMVpV0bbu9Bs5xAyQVprGINAwfja_VhkemEf4Ad9ZOR1Y-LDtI_7-qRVgPZAs1bHaqbPhrp3BSHal4CUauXfHFLBsAar7-7KWZNYgcK7KCRESwTIucPcfmQ';
+        $body = '{
+                "phone_number": "' . $no_tlp_pic . '",
+                "message": {
+                    "type": "template",
+                    "template": {
+                        "template_code_id": "a1b52102_7d78_4d09_9e95_0cec44bea8c7:template_notifikasi_srt_masuk",
+                        "payload": [
+                            {
+                                "position": "header",
+                                "parameters": [
+                                    {
+                                        "type": "image",
+                                        "image": {
+                                                "url": "https://storage.ocatelkom.co.id/image/bUU7Yg2faks6qPm22N2M16112022jpeg.jpeg"
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                "position": "body",
+                                "parameters": [
+                                    {
+                                        "type": "text",
+                                        "text": "' . $nama_pic . '"
+                                    },
+                                    {
+                                        "type": "text",
+                                        "text": "' . $params->dari . '"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }';
+
+        $body = Str::replace("\n", '', $body);
+        $response = Http::withOptions(['verify' => false])->withToken($token)->withBody($body, 'application/json')->post($url);
     }
 }
